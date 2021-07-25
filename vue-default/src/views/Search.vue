51fd6c9dba880e103634297995281bbfc632c008
EUNKYUNG JUNG <thxjek@gmail.com>
2021-07-25 21:06:11 +0900

    [ADD] SaveStudy.vue (SeachStudy.vue 랑 코드가 바뀜)

--- vue-default/src/views/Search.vue
+++ vue-default/src/views/Search.vue
@@ -1,43 +1,9 @@
 <template>
-    <!--조회화면 구현하기-->
     <div>
-        <!--조회조건-->
-        <div>
-            <select v-model="selectedCity">
-                <option
-                    :value="city.code"
-                    :key="i"
-                    v-for="(city, i) in cityList"
-                    >{{ city.name }}</option
-                >
-            </select>
-            <input
-                type="text"
-                v-model="userName"
-                @keyup.enter="searchUserList"
-            />
-            <button type="button" @click="searchUserList">조회</button>
-        </div>
-        <!--조회결과-->
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <!--filed 3개-->
-                        <th>이름</th>
-                        <th>나이</th>
-                        <th>직업</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr :key="i" v-for="(person, i) in userList">
-                        <td>{{ person.name }}</td>
-                        <td>{{ person.age }}</td>
-                        <td>{{ person.job }}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+        <input type="text" v-model="userInfo.name" />
+        <input type="text" v-model.number="userInfo.age" />
+        <input type="text" v-model="userInfo.job" />
+        <button type="button" @click="saveUserInfo">저장</button>
     </div>
 </template>
 <script>
@@ -46,14 +12,15 @@ export default {
     components: {},
     data() {
         return {
-            cityList: [
-                { name: "서울", code: "02" },
-                { name: "부산", code: "21" },
-                { name: "제주", code: "064" },
-            ],
-            selectedCity: "",
-            userName: "",
-            userList: [], //우리가 받아온거 여기에 넣기
+            name: "",
+            age: 0,
+            job: "",
+            userInfo: {
+                //Object로 묶어서 하나로 관리해서 하면 나중에 필드가 복잡해졌을때 관리가 쉽다.
+                name: "",
+                age: 0,
+                job: "",
+            },
         };
     },
     setup() {},
@@ -61,32 +28,33 @@ export default {
     mounted() {},
     unmounted() {},
     methods: {
-        searchUserList() {
-            // const selectedCity = this.selectedCity;
-            // const userName = this.userName;
+        saveUserInfo() {
+            if (this.userInfo.name == "") {
+                return alert("사용자 이름을 입력하세요.");
+            }
 
-            //params == 파라미터
-            const params = {
-                //이 파랄미터를 서버 프로그램쪽으로 HTTP 리퀘스트 던져야되는데, 그럼 그 결과를 받아와서 , 화면에 뿌려준다.
-                selectedCity: this.selectedCity,
-                userName: this.userName,
-            };
-            //서버로부터 받아온 List를 userList []에 넣는다. 그리고, -> 조회 버튼을 클릭 하면, 테이블을 만들어준다.
-            this.userList = this.getUserList(params);
+            if (this.userInfo.age == "" || this.userInfo.age == "") {
+                return alert("나이를 입력하세요.");
+            }
+
+            //    const params = {
+            //        name: this.name,
+            //        age: this.age,
+            //        job: this.job,
+            //    };
+
+            const r = this.saveData(this.userInfo);
+            //서버로 부터 가져온 값이 S이면 '사용자 정보가 생성되었습니다.' 라고 출력해라.
+            if (r == "S") {
+                alert("사용자 정보가 생성되었습니다.");
+            }
         },
 
-        //서버가 없으므로, 가상 함수를 만들어보자! --하드코딩으로 처리함
-        getUserList(params) {
+        //서버랑 통신하는 함수 params를 넘겨준다.
+        saveData(params) {
             console.log(params);
-
-            let list = [
-                { name: "유재석", age: 50, job: "연예인" },
-                { name: "하하", age: 40, job: "연예인" },
-                { name: "양세찬", age: 37, job: "연예인" },
-                { name: "김종국", age: 46, job: "연예인" },
-            ];
-
-            return list;
+            const r = "S"; // result가 Success 로 왔다.
+            return r; //성공했을시, r
         },
     },
 };
