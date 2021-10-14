609c180f6e9728978a30c7428e142af78dda6238
EUNKYUNG JUNG <thxjek@gmail.com>
2021-10-14 23:32:22 +0900

    vue study

---
+++ vue-vuetify/vuetify-template/src/views/Dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>Dashboard Page</div>
+</template>
+<script>
+export default {
+    name: "",
+    components: {},
+    data() {
+        return {
+            sampleData: "",
+        };
+    },
+    setup() {},
+    created() {},
+    mounted() {},
+    unmounted() {},
+    methods: {},
+};
+</script>
---
+++ vue-vuetify/vuetify-template/src/views/GridSystem.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        GridSystemPage
+    </div>
+</template>
+<script>
+export default {
+    name: "",
+    components: {},
+    data() {
+        return {
+            sampleData: "",
+        };
+    },
+    setup() {},
+    created() {},
+    mounted() {},
+    unmounted() {},
+    methods: {},
+};
+</script>
