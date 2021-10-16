83689dc4d7dc4cd49e74e23f1405eaf1c7d09dca
EUNKYUNG JUNG <thxjek@gmail.com>
2021-10-16 13:25:19 +0900

    Add Multiple Layouts | 다중레이아웃 (컴포넌트) 구조

--- vue-vuetify/vuetify-template/src/App.vue
+++ vue-vuetify/vuetify-template/src/App.vue
@@ -1,81 +1,10 @@
 <template>
-  <v-app>
-    <v-app-bar app color="primary" dark>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
-      <v-spacer />
-    </v-app-bar>
-
-    <v-navigation-drawer
-      v-model="drawer"
-      dark
-      app
-      :src="require('@/assets/sidebar.jpg')"
-    >
-      <template v-slot:img="props">
-        <v-img :gradient="gradient" v-bind="props" />
-      </template>
-
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="text-h6">
-            Application
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            subtext
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider />
-
-      <v-list dense nav>
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          link
-          :to="item.to"
-          active-class="primary"
-          class="py-2"
-        >
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-main>
-      <!--router-view 를 감쌌다.-->
-      <v-container fluid>
-        <router-view />
-      </v-container>
-    </v-main>
-  </v-app>
+  <router-view />
 </template>
 
 <script>
+import "@/styles/overrides.sass";
 export default {
-  name: "App",
-
-  data: () => ({
-    drawer: false,
-    gradient: "rgba(0, 0, 0, .7), rgba(0, 0, 0, .7)",
-    items: [
-      { title: "Dashboard", icon: "mdi-view-dashboard", to: "/" },
-      { title: "GridSystem", icon: "mdi-image", to: "/grid-system" },
-      { title: "Breakpoints", icon: "mdi-image", to: "/breakpoints" },
-      { title: "Grid List Page", icon: "mdi-image", to: "/grid-list-page" },
-      { title: "Typography", icon: "mdi-image", to: "/typography" },
-      { title: "Tables", icon: "mdi-image", to: "/tables" },
-      { title: "Forms", icon: "mdi-image", to: "/forms" },
-      { title: "Buttons", icon: "mdi-image", to: "/buttons" },
-      { title: "Icons", icon: "mdi-image", to: "/icons" }
-    ],
-    right: null
-  })
+  name: "App"
 };
 </script>
---
+++ vue-vuetify/vuetify-template/src/views/page/ProductList.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    ProductList
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
